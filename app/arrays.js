d003a8a582946dc60be6699d9e8b94f6b4686108
unknown <mullins.cindy@gmail.com>
2016-10-18 20:55:55 -0700

    my first work

--- app/arrays.js
+++ app/arrays.js
@@ -2,19 +2,28 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.arraysAnswers = {
   indexOf: function(arr, item) {
-
+    return arr.indexOf(item);
   },
 
   sum: function(arr) {
-
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+    return sum;
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(function(item) { return item !== 2 });
   },
 
   removeWithoutCopy: function(arr, item) {
-
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i] !==2) {
+        arr.push([i])
+      }
+        return arr;
+    }
   },
 
   append: function(arr, item) {
